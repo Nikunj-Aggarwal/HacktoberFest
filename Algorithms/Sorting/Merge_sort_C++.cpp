de1a73fc1db2769f506962747ce4953da396627d
shreysingh8838 <zehen8838@gmail.com>
2020-10-21 17:31:46 +0530

    ADD YOUR MESSAGE HERE (shreyHacktoberFest mergesort added)

--- Algorithms/Sorting/Merge_sort_C++.cpp
+++ Algorithms/Sorting/Merge_sort_C++.cpp
@@ -1,95 +1,111 @@
-#include<bits/stdc++.h>
-using namespace std;
-// Merges two subarrays of arr[].
-// First subarray is arr[l..m]
-// Second subarray is arr[m+1..r]
-void merge(int arr[], int l, int m, int r)
-{
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
 
-    /* create temp arrays */
-    int L[n1], R[n2];
+// This code is contributed by Shrey Singh
+// C++ program for Merge Sort 
+#include<iostream> 
+using namespace std; 
 
-    /* Copy data to temp arrays L[] and R[] */
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
+// Merges two subarrays of arr[]. 
+// First subarray is arr[l..m] 
+// Second subarray is arr[m+1..r] 
+void merge(int arr[], int l, int m, int r) 
+{ 
+	int n1 = m - l + 1; 
+	int n2 = r - m; 
 
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        }
-        else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
+	// Create temp arrays 
+	int L[n1], R[n2]; 
 
-    /* Copy the remaining elements of L[], if there
-       are any */
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
+	// Copy data to temp arrays L[] and R[] 
+	for(int i = 0; i < n1; i++) 
+		L[i] = arr[l + i]; 
+	for(int j = 0; j < n2; j++) 
+		R[j] = arr[m + 1 + j]; 
 
-    /* Copy the remaining elements of R[], if there
-       are any */
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
+	// Merge the temp arrays back into arr[l..r] 
+	
+	// Initial index of first subarray 
+	int i = 0; 
+	
+	// Initial index of second subarray 
+	int j = 0; 
+	
+	// Initial index of merged subarray 
+	int k = l; 
+	
+	while (i < n1 && j < n2) 
+	{ 
+		if (L[i] <= R[j]) 
+		{ 
+			arr[k] = L[i]; 
+			i++; 
+		} 
+		else
+		{ 
+			arr[k] = R[j]; 
+			j++; 
+		} 
+		k++; 
+	} 
 
-/* l is for left index and r is right index of the
-   sub-array of arr to be sorted */
-void mergeSort(int arr[], int l, int r)
-{
-    if (l < r) {
-        // Same as (l+r)/2, but avoids overflow for
-        // large l and h
-        int m = l + (r - l) / 2;
+	// Copy the remaining elements of 
+	// L[], if there are any 
+	while (i < n1) 
+	{ 
+		arr[k] = L[i]; 
+		i++; 
+		k++; 
+	} 
 
-        // Sort first and second halves
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
+	// Copy the remaining elements of 
+	// R[], if there are any 
+	while (j < n2) 
+	{ 
+		arr[k] = R[j]; 
+		j++; 
+		k++; 
+	} 
+} 
 
-        merge(arr, l, m, r);
-    }
-}
+// l is for left index and r is 
+// right index of the sub-array 
+// of arr to be sorted */ 
+void mergeSort(int arr[], int l, int r) 
+{ 
+	if (l < r) 
+	{ 
+		
+		// Same as (l+r)/2, but avoids 
+		// overflow for large l and h 
+		int m = l + (r - l) / 2; 
 
-/* UTILITY FUNCTIONS */
-/* Function to print an array */
-void printArray(int A[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++)
-        printf("%d ", A[i]);
-    printf("\n");
-}
+		// Sort first and second halves 
+		mergeSort(arr, l, m); 
+		mergeSort(arr, m + 1, r); 
 
-/* Driver program to test above functions */
-int main()
-{
-    int arr[] = { 1, 5, 4, 3, 2};
-    int arr_size = sizeof(arr) / sizeof(arr[0]);
+		merge(arr, l, m, r); 
+	} 
+} 
 
-    printf("Given array is \n");
-    printArray(arr, arr_size);
+// UTILITY FUNCTIONS 
+// Function to print an array 
+void printArray(int A[], int size) 
+{ 
+	for(int i = 0; i < size; i++) 
+		cout << A[i] << " "; 
+} 
 
-    mergeSort(arr, 0, arr_size - 1);
+// Driver code 
+int main() 
+{ 
+	int arr[] = { 12, 11, 13, 5, 6, 7 }; 
+	int arr_size = sizeof(arr) / sizeof(arr[0]); 
 
-    printf("\nSorted array is \n");
-    printArray(arr, arr_size);
-    return 0;
-}
+	cout << "Given array is \n"; 
+	printArray(arr, arr_size); 
+
+	mergeSort(arr, 0, arr_size - 1); 
+
+	cout << "\nSorted array is \n"; 
+	printArray(arr, arr_size); 
+	return 0; 
+} 
